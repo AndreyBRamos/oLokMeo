c832e5887779ff11c72ed13ef47c9f1f655d1a13
raffixrcr <raffixrcr@hotmail.com>
2015-12-07 23:27:54 -0200

    lua comentada em br

--- bixoLoko/moon.cpp
+++ bixoLoko/moon.cpp
@@ -1,9 +1,3 @@
-/*Published under The MIT License (MIT)
-
-See LICENSE.TXT*/
-
-// Ryan Pridgeon COM2032 rp00091
-
 #include "moon.h"
 #include <cmath>
 
@@ -30,35 +24,34 @@ Moon::Moon(float distanciaPlaneta, float tempoOrbita, float tempoRotacao, float 
 	this->textureHandle = textureHandle;
 }
 
-// Calculate its position in 3d space relative to the planet in the orbit using the given time value
+// calcula a posição da lua ao redor do planeta
 void Moon::calculatePosition(float time)
 {
-	// find the angle of orientation of the orbit around the planet
+	// procura o angulo de rotação ao redor do planeta
 	float angle = time * 3.1419f / tempoOrbita;
 	
-	// use trig to find the position in space relative to the planet
+	// calcula a posição do planeta 
 	position[0] = sin(angle) * distanciaPlaneta;
 	position[1] = cos(angle) * distanciaPlaneta;
 	position[2] = 0;
 
-	// find the rotacao of the moon around its axis
+	// rotação da lua ao redor do eixo
 	//rotacao = time * 360 / tempoRotacao;
     rotacao = 0.0;
 }
 
-// Render it to the screen
 void Moon::render(void)
 {
 	glPushMatrix();
 
-	// bind the moons texture
+	// textura
 	glBindTexture(GL_TEXTURE_2D, textureHandle);
 
-	// translate to the right positon and rotate for the moons spinning
+	// translada para a posição certa e rotaciona
 	glTranslatef(position[0] * escalaDistancia, position[1] * escalaDistancia, position[2] * escalaDistancia);
 	glRotatef(-rotacao, 0.0f, 0.0f, 1.0f);
 	
-	// render as a GLU sphere quadric object
+	// renderiza um obejto quadrico esfera
 	GLUquadricObj* quadric = gluNewQuadric();
 	gluQuadricTexture(quadric, true);
 	gluQuadricNormals(quadric, GLU_SMOOTH);
@@ -68,13 +61,13 @@ void Moon::render(void)
 	glPopMatrix();
 }
 
-// render this planets orbit circle
+// renderiza a orbita da lua ao redor do planeta
 void Moon::renderOrbit(void)
 {
-	// draw a line strip
+	// desenha a linha do planeta
 	glBegin(GL_LINE_STRIP);
 
-	// loop round from 0 to 2*PI and draw around the raio of the orbit using trigonometry
+	// laço de 0 a 2PI e desenha o raio da orbita usando trigonometria 
 	for (float angle = 0.0f; angle < 6.283185307f; angle += 0.1f)
 	{
 		glVertex3f(sin(angle) * distanciaPlaneta * escalaDistancia, cos(angle) * distanciaPlaneta * escalaDistancia, 0.0f);
